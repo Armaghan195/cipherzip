07678733d18cfe3aad447b1f41e2cd4ba239e055
zohad1 <149082887+zohad1@users.noreply.github.com>
2024-12-03 21:25:25 +0500

    Update Encryption.cpp

--- Encryption.cpp
+++ Encryption.cpp
@@ -1,1 +1,16 @@
+#include "Encryption.h"
+#include <iostream>
 
+ListNode* Encryption::createLinkedList(const string& text)
+{
+	ListNode* head = new ListNode(text[0]);
+	ListNode* current = head;
+
+	for (size_t i = 1; i < text.length(); ++i)
+	{
+		current->next = new ListNode(text[i]);
+		current = current->next;
+	}
+
+	return head;
+}
