0b3d755942bcb305e096b2ea4c5a6864c14e34b3
zohad1 <149082887+zohad1@users.noreply.github.com>
2024-12-03 21:24:54 +0500

    Update Encryption.h

--- Encryption.h
+++ Encryption.h
@@ -1,1 +1,26 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+	char data;
+	ListNode* next;
+
+	ListNode(char c) : data(c), next(NULL){}
+
+};
+
+class Encryption
+{
+private:
+	static ListNode* createLinkedList(const string& text);
+	static void deleteLinkedList(ListNode* head);
+};
+
+
+#endif
